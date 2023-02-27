a37e4e0f6253a889bff0cfae622cde2cbb13e314
Jonathan Fung <jonathan626fung@gmail.com>
2023-02-27 07:52:52 -0800

    Episode 9

--- Un-bear-able/Assets/Scripts/MarkerManager.cs
+++ Un-bear-able/Assets/Scripts/MarkerManager.cs
@@ -5,15 +5,23 @@ using UnityEngine.Tilemaps;
 
 public class MarkerManager : MonoBehaviour
 {
-    [SerializeField] Tilemap targetTilemap;
+    public Tilemap targetTilemap;
     [SerializeField] TileBase tile;
     public Vector3Int markedCellPosition;
     Vector3Int oldCellPosition;
+    bool show;
 
     private void Update()
     {
+        if (show == false) { return; }
         targetTilemap.SetTile(oldCellPosition, null);
         targetTilemap.SetTile(markedCellPosition, tile);
         oldCellPosition = markedCellPosition;
+    }
+
+    internal void Show(bool selectable)
+    {
+        show = selectable;
+        targetTilemap.gameObject.SetActive(show);
     }
 }
--- Un-bear-able/Assets/Scripts/ToolsCharacterController.cs
+++ Un-bear-able/Assets/Scripts/ToolsCharacterController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ToolsCharacterController : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class ToolsCharacterController : MonoBehaviour
     [SerializeField] float sizeOfInteractableArea = 1.2f;
     [SerializeField] MarkerManager markerManager;
     [SerializeField] TileMapReadController tileMapReadController;
+    [SerializeField] float maxDistance = 1.5f;
+    [SerializeField] CropsManager cropsManager;
+    [SerializeField] TileData plowableTiles;
+
+    Vector3Int selectedTilePosition;
+    bool selectable;
 
     private void Awake()
     {
@@ -20,20 +27,38 @@ public class ToolsCharacterController : MonoBehaviour
 
     private void Update()
     {
+        SelectTile();
+        CanSelectCheck();
         Marker();
         if (Input.GetMouseButtonDown(0))
         {
-            UseTool();
+            if (UseToolWorld() == true)
+            {
+                return;
+            }
+            UseToolGrid();
         }
+    }
+
+    private void SelectTile()
+    {
+        selectedTilePosition = tileMapReadController.GetGridPosition(Input.mousePosition, true);
+    }
+
+    void CanSelectCheck()
+    {
+        Vector2 characterPosition = transform.position;
+        Vector2 cameraPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        selectable = Vector2.Distance(characterPosition, cameraPosition) < maxDistance;
+        markerManager.Show(selectable);
     }
 
     private void Marker()
     {
-        Vector3Int gridPosition = tileMapReadController.GetGridPosition(Input.mousePosition, true);
-        markerManager.markedCellPosition = gridPosition;
+        markerManager.markedCellPosition = selectedTilePosition;
     }
 
-    private void UseTool()
+    private bool UseToolWorld()
     {
         Vector2 position = rgbd2d.position + character.lastMotionVector * offsetDistance;
 
@@ -45,7 +70,26 @@ public class ToolsCharacterController : MonoBehaviour
             if (hit != null)
             {
                 hit.Hit();
-                break;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void UseToolGrid()
+    {
+        if (selectable == true)
+        {
+            TileBase tileBase = tileMapReadController.GetTileBase(selectedTilePosition);
+            TileData tileData = tileMapReadController.GetTileData(tileBase);
+            if (tileData != plowableTiles) { return; }
+
+            if (cropsManager.Check(selectedTilePosition))
+            {
+                cropsManager.Seed(selectedTilePosition);
+            }
+            else {
+                cropsManager.Plow(selectedTilePosition);
             }
         }
     }
