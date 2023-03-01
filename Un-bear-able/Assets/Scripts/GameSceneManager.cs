838fe2861a812c719bd0501176122f57c8f0098d
Jonathan Fung <jonathan626fung@gmail.com>
2023-03-01 01:08:33 -0800

    20

--- Un-bear-able/Assets/Scripts/GameSceneManager.cs
+++ Un-bear-able/Assets/Scripts/GameSceneManager.cs
@@ -44,6 +44,8 @@ public class GameSceneManager : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
         }
 
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(currentScene));
+
         cameraConfiner.UpdateBounds();
         screenTint.UnTint();
     }
